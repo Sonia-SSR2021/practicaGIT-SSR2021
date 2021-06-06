9b6e84f6cdd595cde00741f6f6370f512dabea5f
Sonia Soriano Rodríguez <soriano20887@iesmarenostrum.com>
2021-06-06 10:52:49 +0200

    cambio justo ahoras

--- WindowsFormsApp1/WindowsFormsApp1/Program.cs
+++ WindowsFormsApp1/WindowsFormsApp1/Program.cs
@@ -17,6 +17,8 @@ namespace WindowsFormsApp1
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
+
+            MessageBox.Show("nuevo");
         }
     }
 }
